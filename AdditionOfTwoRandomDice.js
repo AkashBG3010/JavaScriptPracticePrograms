f83e16a9b47bc488902e524391729c1f8104769f
Akash B G <akashbpura@gmail.com>
2022-01-29 00:22:14 +0530

    Added to perform addition of two random dice

---
+++ AdditionOfTwoRandomDice.js
@@ -0,0 +1,7 @@
+let num1 = (Math.floor(Math.random() *6) +1)
+console.log('Random generated Dice_number_1 is:' +num1)
+let num2 = (Math.floor(Math.random() *6) +1)
+console.log('Random generated Dice_number_2 is:' +num2)
+
+add = num1 + num2
+console.log('Addition of two random dice is: '+add)
