54e1b310961ec54cfc110a21c2f7e7a22308bf29
Akash B G <akashbpura@gmail.com>
2022-01-29 01:10:24 +0530

    Added to perform Unit conversions

---
+++ UnitConversion.js
@@ -0,0 +1,12 @@
+inches = prompt('Enter the value in inches to be converted: ')
+console.log('We Know that 1ft = 12 inches. Therefore, ')
+foot = inches/12
+console.log(inches+' is '+foot+'ft')
+console.log('---------------------------------------------------------')
+
+console.log('The rectangular plot 60ft X 40ft is equal to: ')
+console.log(60*0.3048+'mtrs X '+40*0.3048+'mtrs')
+console.log('---------------------------------------------------------')
+
+console.log('The area of 25 rectangular plot of (60ft X 40ft) is : ')
+console.log(((60*40)*0.00002295684113)*25+'acrs')
