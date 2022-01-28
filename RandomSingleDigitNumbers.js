fd9f97c66ed97749eeabfa9f1e9d36a81ca16165
Akash B G <akashbpura@gmail.com>
2022-01-28 23:21:48 +0530

    Added to get the random single digit number

---
+++ RandomSingleDigitNumbers.js
@@ -0,0 +1,5 @@
+let num1 = (Math.floor(Math.random() * 10))
+console.log('Random single digit number is: ' +num1);
+
+//OR THE OTHER WAY IS:
+//console.log('Random single digit number is: ' +(Math.floor(Math.random() * 10));
