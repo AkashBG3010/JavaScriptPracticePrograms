c488faadfb041e8fb6e202da7cf4f4558b33c2dc
Akash B G <akashbpura@gmail.com>
2022-01-29 00:34:11 +0530

    Added to perform addition and average of five two digit random numbers

---
+++ RandomSumAndAverage.js
@@ -0,0 +1,15 @@
+let num1 = (Math.floor(Math.random() *99) +10)
+console.log('Random generated number_1 is:' +num1)
+let num2 = (Math.floor(Math.random() *99) +10)
+console.log('Random generated number_2 is:' +num2)
+let num3 = (Math.floor(Math.random() *99) +10)
+console.log('Random generated number_3 is:' +num3)
+let num4 = (Math.floor(Math.random() *99) +10)
+console.log('Random generated number_4 is:' +num4)
+let num5 = (Math.floor(Math.random() *99) +10)
+console.log('Random generated number_5 is:' +num5)
+
+add = num1 + num2 + num3 + num4 + num5
+avg = add/5
+
+console.log('Addition of five random two digit number is: '+add+' and its average is: '+avg)
