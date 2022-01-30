167a376cbe7d50a7c8d62eb16b2a4a57ae5ffa09
Akash B G <akashbpura@gmail.com>
2022-01-30 15:00:46 +0530

    Modified to convert numer to word using switch

---
+++ NumberToWordCase.js
@@ -0,0 +1,47 @@
+num = prompt('Enter a number between 0-9 to be converted to word: ')
+num = parseInt(num);
+switch(num) {
+    case 0:
+    console.log('Entered number is "ZERO"')
+    break
+
+    case 1:
+    console.log('Entered number is "ONE"')
+    break
+
+    case 2:
+    console.log('Entered number is "TWO"')
+    break
+
+    case 3:
+    console.log('Entered number is "THREE"')
+    break
+
+    case 4:
+    console.log('Entered number is "FOUR"')
+    break
+
+    case 5:
+    console.log('Entered number is "FIVE"')
+    break
+
+    case 6:
+    console.log('Entered number is "SIX"')
+    break
+
+    case 7:
+    console.log('Entered number is "SEVEN"')
+    break
+
+    case 8:
+    console.log('Entered number is "EIGHT"')
+    break
+
+    case 9:
+    console.log('Entered number is "NINE"')
+    break
+
+    default :
+    console.log('Invalid!! Please enter a number between 0-9')
+    break
+}
