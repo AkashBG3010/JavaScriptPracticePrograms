127789f0d04ea326faf2706a9c616f12a211b34f
Akash B G <akashbpura@gmail.com>
2022-01-30 13:29:17 +0530

    Modified to convert numer to word

--- NumberToWord.js
+++ NumberToWord.js
@@ -1,7 +1,10 @@
-num = prompt('Enter a number between 1-5 to be converted to word: ')
+num = prompt('Enter a number between 0-9 to be converted to word: ')
 
-if (num == 1){
-console.log('Entered number is "ONE"')
+if (num == 0){
+console.log('Entered number is "ZERO"')
+}
+else if (num == 1){
+    console.log('Entered number is "ONE"')
 }
 else if (num == 2){
     console.log('Entered number is "TWO"')
@@ -15,6 +18,18 @@ else if (num == 4){
 else if (num == 5){
     console.log('Entered number is "FIVE"')
 }
+else if (num == 6){
+    console.log('Entered number is "SIX"')
+}
+else if (num == 7){
+    console.log('Entered number is "SEVEN"')
+}
+else if (num == 8){
+    console.log('Entered number is "EIGHT"')
+}
+else if (num == 9){
+    console.log('Entered number is "NINE"')
+}
 else {
-    console.log('Invalid!! Please enter a number between 1-5')
+    console.log('Invalid!! Please enter a number between 0-9')
 }
