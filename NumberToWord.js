a567f08698ab369a549b0d1e5362cbf832954002
Akash B G <akashbpura@gmail.com>
2022-01-30 13:24:15 +0530

    Added to convert numer to word

---
+++ NumberToWord.js
@@ -0,0 +1,20 @@
+num = prompt('Enter a number between 1-5 to be converted to word: ')
+
+if (num == 1){
+console.log('Entered number is "ONE"')
+}
+else if (num == 2){
+    console.log('Entered number is "TWO"')
+}
+else if (num == 3){
+    console.log('Entered number is "THREE"')
+}
+else if (num == 4){
+    console.log('Entered number is "FOUR"')
+}
+else if (num == 5){
+    console.log('Entered number is "FIVE"')
+}
+else {
+    console.log('Invalid!! Please enter a number between 1-5')
+}
